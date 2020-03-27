de910078a5269d4b1fefcc237c4a2b687ce24dbe
Toskin Nikolay <itonik@users.noreply.github.com>
2020-03-27 21:56:30 +0300

    Exceptions for lessons controller/model

--- controller/lessons.py
+++ controller/lessons.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, request
 from flasgger import swag_from
 import model.lessons
+from model.exceptions import NoSuchDBRecordException
 
 lessons = Blueprint('lessons',  __name__, url_prefix='/lessons')
 
@@ -8,9 +9,15 @@ lessons = Blueprint('lessons',  __name__, url_prefix='/lessons')
 @swag_from('swagger/lessons_student.yml')
 def get_lessons_for_student(student_id):
     # TODO: request.args.get('date-from'), request.args.get('date-to')
-    return model.lessons.for_student(student_id)
+    try:
+        return model.lessons.for_student(student_id)
+    except NoSuchDBRecordException:
+        return 'Not Found', 404
 
 @lessons.route('/group/<int:group_id>')
 @swag_from('swagger/lessons_group.yml')
 def get_lessons_for_group(group_id):
-    return model.lessons.for_group(group_id)
+    try:
+        return model.lessons.for_group(group_id)
+    except NoSuchDBRecordException:
+        return 'Not Found', 404
--- model/lessons.py
+++ model/lessons.py
@@ -2,6 +2,7 @@ from peewee import JOIN
 from db import Lesson, Group, StudentToGroup, Room, Lecturer, Student
 from playhouse.shortcuts import model_to_dict
 from flask import jsonify
+from model.exceptions import NoSuchDBRecordException
 
 # TODO: maybe run some perfomance tests
 
@@ -20,6 +21,12 @@ def query_to_json_mapper(query):
     ])
 
 def for_student(student_id):
+    # Check if student exists
+    try:
+        Student.get_by_id(student_id)
+    except Group.DoesNotExist:
+        raise NoSuchDBRecordException("Student with id {} does not exist in database".format(student_id))
+
     query = (Lesson
         .select(Lesson.name, Lesson.start, Lesson.end, Lesson.date, Room.name, Room.address, Group.name)
         .join(Group)
@@ -33,6 +40,12 @@ def for_student(student_id):
     return query_to_json_mapper(query)
 
 def for_group(group_id, date_frame=(None, None)):
+    # Check if group exists
+    try:
+        Group.get_by_id(group_id)
+    except Group.DoesNotExist:
+        raise NoSuchDBRecordException("Group with id {} does not exist in database".format(group_id))
+
     query = (Lesson
         .select(Lesson.name, Lesson.start, Lesson.end, Lesson.date, Room.name, Room.address, Group.name)
         .join(Group).switch(Lesson)

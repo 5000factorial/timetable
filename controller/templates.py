8dfc1cb88c6243fe2336cb474b0fd1b9ab43307d
Toskin Nikolay <itonik@users.noreply.github.com>
2020-03-28 19:39:33 +0300

    Случайно залитый файл

--- controller/templates.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint, render_template
-"""
-templates = Blueprint('templates',  __name__)
-
-@templates.route('/')
-def index():
-    return render_template("index.html")
-"""
